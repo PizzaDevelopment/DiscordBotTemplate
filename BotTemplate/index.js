9c933fef57e5c19fd1f417fc45a242baddfd5d98
PizzaDevelopment <70071185+PizzaDevelopment@users.noreply.github.com>
2020-08-23 00:20:43 +0200

    Update index.js

--- BotTemplate/index.js
+++ BotTemplate/index.js
@@ -9,13 +9,16 @@ client.once('ready', () => {
 client.on('message', message => {
 	if (message.content === `${prefix}ping`) {
 	    message.channel.send('Pong!');
-}   else if (message.content.startsWith(`${prefix}lol`)) {
+        }       
+	else if (message.content.startsWith(`${prefix}lol`)) {
 	    message.channel.send('lul');
-}   else if (message.content === `${prefix}server`) {
+        }       
+	else if (message.content === `${prefix}server`) {
 	    message.channel.send(`**Server Name:** ${message.guild.name}\n**Members:** ${message.guild.memberCount}\n**Created At:** ${message.guild.createdAt}\n**Region:** ${message.guild.region}`);
-}   else if (message.content === `${prefix}user-info`) {
+        }       
+	else if (message.content === `${prefix}user-info`) {
 	    message.channel.send(`**Your username:** ${message.author.username}\n**Your ID:** ${message.author.id}`);
-}
+        }
 
 });
 
